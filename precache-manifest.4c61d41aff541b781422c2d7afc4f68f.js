26e0753c30dbcb51fddc4611a37d7fec49ff6124
Paul Ccari <paulclindo@gmail.com>
2020-05-27 21:33:25 -0500

    Updates

---
+++ precache-manifest.4c61d41aff541b781422c2d7afc4f68f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca31b45a251b3d9a5fd5aa859f9a66e",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "804de25cb5a43e6334ed",
+    "url": "/react-blog-github/static/css/main.fa16faf3.chunk.css"
+  },
+  {
+    "revision": "7170bcc2e6d24256d511",
+    "url": "/react-blog-github/static/js/2.7f1725e1.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog-github/static/js/2.7f1725e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "804de25cb5a43e6334ed",
+    "url": "/react-blog-github/static/js/main.2c69ee9d.chunk.js"
+  },
+  {
+    "revision": "8216af6db86cb58257df",
+    "url": "/react-blog-github/static/js/runtime-main.b7e97252.js"
+  },
+  {
+    "revision": "65f8709df0f0bfa7d0a05d7f9517ba07",
+    "url": "/react-blog-github/static/media/loader.65f8709d.svg"
+  }
+]);
