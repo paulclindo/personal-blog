7e742f2df6c2cb05839650d02134feb8936d7bbc
Paul Ccari <paulclindo@gmail.com>
2020-06-27 03:41:41 -0500

    Updates

---
+++ precache-manifest.158aacaae75522609bfa8e203761f540.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ea596b290afc29244ebc524e9dda91",
+    "url": "/personal-blog/index.html"
+  },
+  {
+    "revision": "b8295a74a23433147297",
+    "url": "/personal-blog/static/css/main.750b9953.chunk.css"
+  },
+  {
+    "revision": "f74198573d46ae35ee71",
+    "url": "/personal-blog/static/js/2.55b4d8f8.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/personal-blog/static/js/2.55b4d8f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8295a74a23433147297",
+    "url": "/personal-blog/static/js/main.5696ad74.chunk.js"
+  },
+  {
+    "revision": "bfdc1dfcbe0a61ff2559",
+    "url": "/personal-blog/static/js/runtime-main.cda9eef4.js"
+  },
+  {
+    "revision": "65f8709df0f0bfa7d0a05d7f9517ba07",
+    "url": "/personal-blog/static/media/loader.65f8709d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-blog-github/precache-manifest.4bc756d5285fe580cf46b91ada0717b6.js"
+  "/personal-blog/precache-manifest.158aacaae75522609bfa8e203761f540.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-blog-github/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
